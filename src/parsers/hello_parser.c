647166894225c47f5392818dd752a31fbe3631cb
lucas <lugomez@itba.edu.ar>
2022-06-08 18:07:39 -0300

    Working with oarsers

--- src/parsers/hello_parser.c
+++ src/parsers/hello_parser.c
@@ -17,3 +17,55 @@ enum hello_state parse_char(uint8_t c, struct hello_parser * parser) {
     }
     return state;
 }
+
+void hello_parser_init(struct hello_parser *hp) {
+    hp->state = hello_reading_version;
+    hp->methods = NULL;
+    hp->index = 0;
+}
+
+enum hello_state consume_hello_byte(uint8_t c, struct hello_parser * hp, struct hello_st * hello_st){
+    switch(hp->state) {
+        case hello_reading_version:
+            if(c == PROXY_SOCKS5_V5){
+                hp->state = hello_reading_nmethods;
+            } else {
+                hp->state = hello_error;
+            }
+            break;
+        case hello_reading_nmethods:
+            if(c <= 0) {
+                hp -> state = hello_error;
+            } else {
+                hp->nmethods= c;
+                hp->methods = calloc(c, sizeof(c));
+                if(hp->methods == NULL) {
+                    hp->state = hello_error;
+                    return hp->state;
+                }
+                hp->state = hello_reading_methods;
+            }
+            break;
+        case hello_reading_methods:
+
+            break;
+        case hello_end:
+            break;
+        case hello_error:
+            break;
+        case default:
+            break;
+    }
+}
+
+enum hello_state consume_hello(buffer * b, struct hello_parser * hp, struct hello_st * hello_st) {
+    enum hello_state state = hp->state;
+
+    while(buffer_can_read(b)) {
+        const uint8_t c = buffer_read(b);
+        state = consume_hello_byte(c, hp, hello_st);
+
+    }
+    state
+    //TODO: PARSE CHAR
+}
--- src/parsers/includes/hello_parser.h
+++ src/parsers/includes/hello_parser.h
@@ -3,42 +3,26 @@
 
 #include <stdlib.h>
 
+#define PROXY_SOCKS5_V5     0x05
 
 enum hello_state {
     hello_reading_version,
     hello_reading_nmethods,
     hello_reading_methods,
-    hello_end
-};
-
-static const struct parser_state_transition hello_reading_version [] =  {
-    {.when = '5',        .dest = S0,        .act1 = foo,},
-    {.when = 'f',        .dest = S0,        .act1 = foo,},
-    {.when = ANY,        .dest = S1,        .act1 = bar,},
-};
-
-#define N(x) (sizeof(x)/sizeof((x)[0]))
-
-
-static const size_t states_n [] = {
-    N(ST_S0),
-    N(ST_S1),
-};
-
-static struct parser_definition definition = {
-    .states_count = N(hello_state),
-    .states       = hello_state,
-    .states_n     = states_n,
-    .start_state  = S0,
+    hello_end,
+    hello_error
 };
 
 
 struct hello_parser {
     enum hello_state state;
     uint8_t * methods;
+    uint8_t nmethods;
     uint8_t index;
 };
 
 enum hello_state consume_buffer(buffer * b, struct hello_parser * parser);
+
+
 
 #endif
