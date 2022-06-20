fbe15c88b7b31885c91fb57cdca7205c141d3741
rodrifera <rodrigonestorfera@gmail.com>
2022-06-20 18:58:22 -0300

    modified man socks5dctl

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-seq 1 250 | parallel -j 0 -I{} curl -x socks5h://127.0.0.1:1080  https://discord.com/api/download?platform=linux&format=deb | sha256sum
+seq 1 500 | parallel -j 0 -I{} curl -x socks5h://127.0.0.1:1080 "http://br.mirror.archlinux-br.org/iso/2022.06.01/archlinux-2022.06.01-x86_64.iso" | sha256sum
 
